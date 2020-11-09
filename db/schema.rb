b96b3c74758975d16d08b990ad0f48620e44cfd8
David Nugent <davidn@uniquode.io>
2020-11-10 06:30:50 +1100

    switch to puma, upgrade web-console and fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_021450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "todo_items", id: :serial, force: :cascade do |t|
-    t.integer "todo_list_id"
+  create_table "todo_items", force: :cascade do |t|
+    t.bigint "todo_list_id"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_021450) do
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
-  create_table "todo_lists", id: :serial, force: :cascade do |t|
+  create_table "todo_lists", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
